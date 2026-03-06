63354d59cfcecab0513a93eec22c8863afe4b63e
EstarinAzx <khitarashi@gmail.com>
2026-03-06 22:32:25 +1300

    Muted language badge color, fix search icon alignment

--- src/webview/components/SearchBar.tsx
+++ src/webview/components/SearchBar.tsx
@@ -11,18 +11,20 @@ export function SearchBar({ query, onQueryChange }: SearchBarProps) {
     const inputRef = useRef<HTMLInputElement>(null);
 
     return (
-        <div class="px-4 pb-3 relative">
-            <span class="absolute left-6 top-1/2 -translate-y-1/2 pointer-events-none" style={{ color: 'var(--vscode-descriptionForeground)' }}>
-                <SearchIcon />
-            </span>
-            <input
-                ref={inputRef}
-                type="text"
-                class="kodo-input pl-8"
-                placeholder="Search snippets…"
-                value={query}
-                onInput={(e) => onQueryChange((e.target as HTMLInputElement).value)}
-            />
+        <div class="px-4 pb-3">
+            <div class="relative flex items-center">
+                <span class="absolute left-2.5 pointer-events-none flex items-center" style={{ color: 'var(--vscode-descriptionForeground)' }}>
+                    <SearchIcon />
+                </span>
+                <input
+                    ref={inputRef}
+                    type="text"
+                    class="kodo-input pl-8"
+                    placeholder="Search snippets…"
+                    value={query}
+                    onInput={(e) => onQueryChange((e.target as HTMLInputElement).value)}
+                />
+            </div>
         </div>
     );
 }
